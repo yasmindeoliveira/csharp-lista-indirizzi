4162288538b10e21fea86f85888cebb5c9ee64e0
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-12-12 15:26:49 +0100

    creata classe indirizzo

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    public class Indirizzo
+    {
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private string zip;
+
+        //Costruttore
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+StreamReader file = File.OpenText("C:\\Users\\generation\\Desktop\\Esercizi GIT\\C#_DotNET\\ListaIndirizzi\\ListaIndirizzi\\File\\addresses.csv");
+
+while (!file.EndOfStream)
+{
+    string line = file.ReadLine();
+    Console.WriteLine(line);
+
+}
+file.Close();
